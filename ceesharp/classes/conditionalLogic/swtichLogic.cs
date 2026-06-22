eec834a3ea924b4e66186ec1f9549fd07942600b
Rohan Khan <107738044+itsrohanhere@users.noreply.github.com>
2026-06-22 05:26:18 +0000

    added branches and loops

---
+++ ceesharp/classes/conditionalLogic/swtichLogic.cs
@@ -0,0 +1,14 @@
+
+string day = "Monday";
+    switch (day)
+{
+    case "Monday":
+        Console.WriteLine("Start of week");
+        break;
+    case "Friday":
+        Console.WriteLine("Weekend soon");
+        break;
+    default:
+        Console.WriteLine("Other day");
+        break;
+}
